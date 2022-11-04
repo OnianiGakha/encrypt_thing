6578c3d3729e3a523607ed96c5042d6b691dcd05
OnianiGakha <onianigakha@gmail.com>
2022-11-04 22:25:58 +0400

    completed

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import math
 generated = False
 grid_layout = []
 proper_layout = ""
+input_layout = []
 while True:
     b = input()
     i=0
     proper_layout = ""
     used = []
-    def generate(): #akdaskdladajdaldsaldadalkjaldkajdaldalkdjalkdjadjaldkjalkdjalkdjajdalkjdalkjdaljda
+    def generate():
         def ogg(num):
             numx = num%8
             if used == []:
-                return True #7 0   0 7
+                return True
             numy = math.floor(num/8)
             for p in used:
                 px = p%8
@@ -22,21 +23,23 @@ while True:
                 if (numy == 7-py and numx == 7-px) or (numx == 7 - px and numy == py) or (numy == 7 - py and numx == px) or (numx == py and numy == 7 - px) or (numy == px and numx == 7 - py):
                     return False
                 else:
-                    print("P = " + str(p) + " " + str(px + 1) + " " + str(py + 1)) # 2 7    0 2
+                    print("P = " + str(p) + " " + str(px + 1) + " " + str(py + 1))
             return True
         for i in range(0,64):
-            grid_layout.append("#") #1 6    6 1
+            input_layout.append("▨")
+            grid_layout.append("▨") 
         for b in range(0,16):
             rand = random.randint(0,63)    
             while True:
-                if used.__contains__(rand): #6 0    7 6  
+                if used.__contains__(rand): 
                     rand = random.randint(0,63)
                 else:
                     if ogg(rand) == True:
                         break
                     else:
                         rand = random.randint(0,63)
-            grid_layout[rand] = "!"
+            input_layout[rand] = "*"
+            grid_layout[rand] = "*"
             used.append(rand)
 
 
@@ -44,42 +47,77 @@ while True:
         msg = list(msg)
         l = 0
         for j in grid_layout:
-            if j == "!" or j!="!" and j!="#":
+            if j == "*" or j!="*" and j!="▨":
                 if msg != []:
                     if new == True:
-                        grid_layout[l] = msg[0]
+                        input_layout[l] = msg[0]
                         msg.remove(msg[0])
                     else:
-                        grid_layout[l] = msg[0]
+                        input_layout[l] = msg[0]
                         msg.remove(msg[0])
                 else:
-                    grid_layout[l] = "#"
+                    input_layout[l] = "▨"
             l+=1
 
+    def rotate():
+        for i in range(0,64):
+            if grid_layout[i] == "*":
+                
+                grid_layout[i] = "▨"
+                ix = i%8
+                iy = math.floor(i/8)
+                
+                newy = ix
+
+                
+                newx = 7 - iy
+                grid_layout[newy * 8 + newx] = "l"
+            
+        for k in range(0,64):
+            if grid_layout[k] == "l":
+                grid_layout[k] = "*"
+
     if b == "generate":
+        input_layout = []
         grid_layout = []
         generate()
         generated = True
         msg = input("encrypt: ")
-        encrypt(msg,False) #1 7    8 2
+        encrypt(msg,False)
     
-    if b == "rotate" and generated == True:
-        pass
+    if b == "rotate":
+        rotate()
 
     if b == "encrypt" and generated == True:
         msg = input("encrypt: ")
         encrypt(msg,True)
 
     proper_layout = ""
-    for g in grid_layout:
+    for g in range(0,len(input_layout)):
         i+=1
-        if i == 8:
-            proper_layout += str(g) + "\n"
-            i=0
-        else:
-            proper_layout += str(g) + " "
-        
-        
+        if grid_layout[g] == "*":
+            if input_layout[g] != "▨":
 
+                if i == 8 :
+                    proper_layout += str(input_layout[g]) + "\n"
+                    i=0
+                else:
+                    proper_layout += str(input_layout[g]) + " "
+            else:
+
+                if i == 8 :
+                    proper_layout += "*" + "\n"
+                    i=0
+                else:
+                    proper_layout += "*" + " "
+        else:
+            if i == 8 :
+                proper_layout += "▨" + "\n"
+                i=0
+            else:
+                proper_layout += "▨" + " "
+        
+        
     print(proper_layout)
+    print(input_layout)
     print(grid_layout)
